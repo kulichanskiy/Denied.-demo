d38510b09c6a65192d7672a442ea8c1951b4ed05
kulichanskiy <kulichanskiy.js@gmail.com>
2025-04-09 17:06:30 -0700

    version 0.0.1 init

---
+++ version 0.0.1/highlight scripts/WindowRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WindowRotation : MonoBehaviour
+{
+    public GameObject window; // Ваш объект окна
+    private Transform pivot; // Пустой объект
+
+    void Start()
+    {
+        // Создаем пустой объект и устанавливаем его как родительский
+        pivot = new GameObject("RotationPivot").transform;
+        pivot.position = window.transform.position + new Vector3(window.transform.localScale.x / 2, -window.transform.localScale.y / 2, 0);
+        window.transform.SetParent(pivot, true);
+    }
+
+    void Update()
+    {
+        // Вращаем пустой объект
+        pivot.Rotate(Vector3.up * Time.deltaTime * 20); // Пример вращения
+    }
+}
